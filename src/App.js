eef48f8f743a5d9043dc76447e9db6eff8b7d9ae
Joules404 <joules.8122@gmail.com>
2023-10-20 15:15:57 -0400

    small modifications front end functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-
-
 function App() {
-
   const randomSentence =
     "In the heart of a bustling testing city, people rush to work, while the sun casts a warm glow on the tall buildings." +
     "Coffee shops line the streets, offering a brief respite from the daily grind."
 
-
   return (
     <div className="App">
-      <Body sentence={randomSentence} />
+      <Body />
     </div>
   );
-} //testing right now again
+} 
 
 
 
-const Body = function (props) {
+const Body = function () {
   const [duration, setDuration] = useState(60);
   const [timeLeft, setTimeLeft] = useState(60);
   const [timerActive, setTimerActive] = useState(false);
@@ -27,33 +23,55 @@ const Body = function (props) {
   const [readOnly, setReadOnly] = useState(false)
   const [desiredTime, setDesiredTime] = useState(60); 
   const [wordCount, setWordCount] = useState(0);
-  const [incorrect, setIncorrect] = useState(0);
-  const [wpm, setWPM] = useState(0); 
+  const [includePunctuation, setIncludePunctuation] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [newWords, setNewWords] = useState(false)
+
+  const [randomWords, setRandomWords] = useState('')
+  const [numberOfDesiredWords, setNumberOfDesiredWords] = useState(15)
+  useEffect(()=>{
+    fetch(`https://random-word-api.herokuapp.com/word?number=${numberOfDesiredWords}`)
+      .then((response)=>response.json())
+      .then((data)=>{
+        setRandomWords(data.join(' '))
+      })
+
+  },[numberOfDesiredWords,newWords])
+  var correct = 0;
+  var incorrect = 0;
+  useEffect(() => {
+    const words = inputValue.trim().split(/\s+/);
+    setWordCount(words.length);
+    const elements = compareTexts(inputValue, randomWords);
+}, [inputValue, randomWords]);
+
   const restart = () => {
     setInputValue('');
     setReadOnly(false)
-    setDuration(60)
-    setTimeLeft(60)
+    setDuration(desiredTime)
+    setTimeLeft(desiredTime)
     setTimerActive(false)
-    setDesiredTime(60)
+    setDesiredTime(desiredTime)
+    document.getElementById("words").value='';
     document.getElementById("timer").value='';
-
   }
   useEffect(() => {
     let timerInterval;
-
     if (timerActive && timeLeft > 0) {
       timerInterval = setInterval(() => {
         setTimeLeft((prevTime) => prevTime - 1);
       }, 1000);
     } else if (timeLeft === 0) {
-      clearInterval(timerInterval);
-      setTimerActive(false);
-      ////display WPM
-      if (wordCount > 0 && (duration - timeLeft) > 0) {
-        const netWPM = ((wordCount - incorrect) / ((duration - timeLeft) / 60)).toFixed(2);
-        setWPM(netWPM);
+      if(timerInterval){
+        clearInterval(timerInterval);
       }
+      setTimerActive(false);
+      setReadOnly(true)
+      const allTypedEntries = correct + incorrect;
+      const uncorrectedErrors = incorrect;
+      const time = duration / 60;
+      const netwpm = ((allTypedEntries/5)-uncorrectedErrors)/time;
+      alert(Math.round(netwpm))
     }
 
     return () => {
@@ -81,63 +99,63 @@ const Body = function (props) {
   }
   function compareTexts(inputText, sentence) {
     const elements = [];
-    var correct = 0;
-    var incorrect_=0;
-    let i = 0;
-    for (i = 0; i < sentence.length; i++) {
+    for (let i = 0; i < sentence.length; i++) {
       const char = sentence[i];
       if (i < inputText.length) {
         if (inputText[i] === char) {
           correct++;
           elements.push(
-            <span key={i} style={{ color: "green", textDecoration:"underline"}}>
+            <span key={i} style={{ color: "green", textDecoration: "underline" }}>
               {char}
             </span>
           );
         } else {
-          incorrect_++;
+          incorrect++;
           elements.push(
-            <span key={i} style={{ color: "red" , textDecoration:"underline"}}>
+            <span key={i} style={{ color: "red", textDecoration: "underline" }}>
               {char}
             </span>
           );
         }
       } else {
         elements.push(
-          <span key={i} style={{ color: "black"}}>
+          <span key={i} style={{ color: "black" }}>
             {char}
           </span>
         );
       }
     }
-    // if(correct+incorrect_=== 0) {
-    //   console.log("hello world")
-    // }else{
-    //   console.log("correct: " + correct)
-    //   console.log("incorrect: " + incorrect_)
-    //   var time = duration - timeLeft;
-    //   wpm = Math.floor(wordCount / (time/60));
-    // }
-    // setIncorrect(incorrect_);
-    // const netWPM = ((wordCount - incorrect) / ((duration - timeLeft) / 60)).toFixed(2);
-    // setWPM(netWPM);
     return elements;
   }
+  
+
   return (
     <div class="App">
       <div class="wrapper">
         <p><a href='#'>Sign in</a> | <a href='#'>Register</a></p>
-        <input type="text" placeholder="WORDS" readOnly></input>
-        <input type="number" placeholder="DESIRED TIME (secs.)" onChange={updateTimer} id="timer" ></input>
-        <input type="text" placeholder="PUNCTUATION" readOnly></input>
-        <input type="text" placeholder="NUMBERS" readOnly></input>
-    </div>
+        <input id = 'words'type="text" placeholder="WORDS (MAX 500)" onInput={(e)=>{
+          e.target.value = e.target.value.replace(/[^0-9]/, '')
+          if(e.target.value === ''){
+            setNumberOfDesiredWords(30)
+          }else setNumberOfDesiredWords(e.target.value)
+        }} autoComplete='new-password'></input>
+        <input type="text" placeholder="TIME (60 seconds)" onChange={updateTimer} id="timer" onInput={(e)=>{
+          e.target.value = e.target.value.replace(/[^0-9]/, '')
+        }}></input>
+        <input type="text" placeholder={includePunctuation ? 'PUNCTUATION (ON)' : 'PUNCTUATION (OFF)'} readOnly onClick={()=>{
+          setIncludePunctuation(!includePunctuation)
+        }}></input>
+        <input type="text" placeholder={includeNumbers ? 'NUMBERS (ON)' : 'NUMBERS (OFF)'} readOnly onClick={()=>{
+          setIncludeNumbers(!includeNumbers)
+        }}></input>
+      </div>
     <div class = "textarea-container">
       <div
         className="custom-textarea"
         style={{ whiteSpace: "pre-wrap", wordWrap: "break-word" }}
       >
-        {compareTexts(inputValue, props.sentence)}
+        {compareTexts(inputValue, randomWords)}
+
       </div>
     </div>
       <input
@@ -149,7 +167,7 @@ const Body = function (props) {
         onChange={(e) => {
           setInputValue(e.target.value)
           handleStart();
-          compareTexts(e.target.value,props.sentence)
+          compareTexts(e.target.value,randomWords)
           var words = e.target.value.trim().split(/\s+/);
           setWordCount(words.length);
         }}
@@ -159,17 +177,17 @@ const Body = function (props) {
 
       <br></br>
       <div class = "wrapper">
+      <button onClick={()=>{
+        setNewWords(!newWords)
+      }}>Generate New Words</button>
       <button onClick={restart}>Restart</button>
       <button onClick={() => {
        setReadOnly(true);
        handleStop();
-       const netWPM = ((wordCount - incorrect) / (duration - timeLeft) * 60).toFixed(2);
       }}>Submit</button>
       </div>
       <div>
         <p style={{fontSize:"40px"}}>Time Left: {timeLeft}</p>
-        {/* <p style={{fontSize:"20px"}}>WPM: {wpm}</p> */}
-
       </div>
 
     </div>
